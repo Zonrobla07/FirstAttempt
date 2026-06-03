c882e8e3bfd7c847b3d8fa438e05cca3674448ec
Alwin Albornoz <alwinenmanuel14@gmail.com>
2026-06-03 00:31:16 -0400

    se agrego includes/footer.php y el index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+// Incluimos la cabecera (Header)
+include 'includes/header.php'; 
+?>
+
+<div>
+    <h1>Bienvenido al Sistema de Control de Inventario</h1>
+    <p style="margin-top: 15px;">Seleccione una opción en el menú superior para comenzar a gestionar los productos, compras y ventas.</p>
+</div>
+
+<?php 
+// Incluimos el pie de página (Footer)
+include 'includes/footer.php'; 
+?>
